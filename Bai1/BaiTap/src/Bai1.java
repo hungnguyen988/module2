040d1ea0039cd80c2f3fc450ad27ea8720cc9e67
hungnguyen988 <hungnguuen988@gmail.com>
2024-09-09 09:56:57 +0700

    nop bai tap

---
+++ Bai1/BaiTap/src/Bai1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("mời nhập tên");
+        String name = input.nextLine();
+        System.out.println("hello " + name);
+    }
+}
